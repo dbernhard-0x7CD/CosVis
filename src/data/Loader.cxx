16cebf976bd10112e75d86aaac0a5a0ac7d72f8e
dbernhard <dbernhard@ethz.ch>
2023-04-26 01:27:24 +0200

    Fix timestep loading on macos

--- src/data/Loader.cxx
+++ src/data/Loader.cxx
@@ -20,13 +20,14 @@ load_cosmology_dataset(std::string data_folder_path) {
   const std::regex vtk_cosmo_file(".*Full\\.cosmo\\.([\\d]{3})\\.vtp$");
   std::map<int, fs::__cxx11::path> ts_to_path;
 
-  std::cmatch base_match;
+  std::smatch base_match;
   for (auto const &dir_entry : fs::directory_iterator{folder}) {
-    if (std::regex_match(dir_entry.path().string().c_str(), base_match,
+    std::string path = dir_entry.path().c_str();
+    if (std::regex_search(path, base_match,
                          vtk_cosmo_file)) {
-      auto number = base_match[1];
-      ts_to_path.insert_or_assign(std::atoi(number.str().c_str()),
-                                  dir_entry.path());
+      auto number = base_match.str(1);
+      int num = std::atoi(number.c_str());
+      ts_to_path.insert_or_assign(num, dir_entry.path());
     }
   }
 
