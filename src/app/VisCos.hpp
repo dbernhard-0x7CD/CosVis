ec9da41660483eb6d3a47303cf7035a30f6d4719
dbernhard <dbernhard@ethz.ch>
2023-05-22 01:58:54 +0200

    Formatting

--- src/app/VisCos.hpp
+++ src/app/VisCos.hpp
@@ -86,16 +86,23 @@ private:
   vtkNew<vtkSliderWidget> timeSliderWidget;
 
   // Filters
+  TempFilterParams temperatureFilterParams;
   vtkNew<vtkProgrammableFilter> temperatureFilter;
+
+  AssignClusterParams clusterFilterParams;
   vtkNew<vtkProgrammableFilter> clusterFilter;
+
+  particleTypeFilterParams particleFilterParams;
   vtkNew<vtkProgrammableFilter> particleTypeFilter;
+
+  // Various
   vtkNew<vtkGlyph3D> glyph3D;
   vtkNew<vtkCamera> camera;
   vtkNew<KeyPressInteractorStyle> keyboardInteractorStyle;
+  vtkNew<vtkSPHQuinticKernel> kernel;
+  vtkNew<vtkStructuredPoints> source;
+  vtkNew<vtkSPHInterpolator> interpolator;
 
-  TempFilterParams temperatureFilterParams;
-  AssignClusterParams clusterFilterParams;
-  particleTypeFilterParams particleFilterParams;
   vtkNew<TimeSliderCallback> timeSliderCallback;
   vtkNew<ResizeWindowCallback> resizeCallback;
 
